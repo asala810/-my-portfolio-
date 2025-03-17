41db6e22ee42c5c8d5e3c2888975046605d3a1f7
asala810 <asalashawkyhana810@gmail.com>
2025-03-17 15:53:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log('basic calculater :');
+
+console.log('*************************');
+
+function add(x,y){
+    return x+y ;
+    
+}
+
+let addition =add(15,5);
+
+console.log(addition);
+
+console.log('*************************');
+
+function sub(x,y){
+    return x-y ;
+}
+let subtraction =sub(15,5);
+console.log(subtraction);
+
+console.log('*************************');
+
+function multi(x,y){
+    return x*y ;
+}
+let multiplication =multi(15,5);
+console.log(multiplication);
+
+console.log('*************************');
+
+function div(x,y){
+    return x/y ;
+}
+let division =div(15,5);
+console.log(division);
